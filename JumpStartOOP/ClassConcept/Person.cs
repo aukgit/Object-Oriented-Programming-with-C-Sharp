bd3d3d4f15fcba1ce70c9a208cb93c69032139ec
Alim Ul Karim <auk.junk@live.com>
2015-01-29 03:09:43 +0600

    class-concept

---
+++ JumpStartOOP/ClassConcept/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpStartOOP.ClassConcept
+{
+    class Person {
+
+        #region data  or attributes
+
+        public string FirstName;
+        public string LastName;
+        public DateTime DateOfBirth;
+        public bool Gender;
+
+        #endregion
+
+        #region Behaviours or Functions or Methods
+
+        public void Walk() {
+            Console.WriteLine(FirstName + " is walking.");
+        }
+
+        public void Eat() {
+            Console.WriteLine(FirstName + " is eating.");
+        }
+
+        public void Talk() {
+            Console.WriteLine(FirstName + " is talking.");
+        }
+
+        #endregion
+
+    }
+}
--- JumpStartOOP/Program.cs
+++ JumpStartOOP/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JumpStartOOP.ClassConcept;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,16 @@ namespace JumpStartOOP
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
+
+            Person person = new Person();
+
+            person.FirstName = "Alice";
+            person.Walk();
+            person.Talk();
+            person.Eat();
+
+            Console.ReadLine();
         }
     }
 }
